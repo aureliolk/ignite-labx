33bf1c3738ae231ec8316f4cb920045d3bf6dbd0
Aurélio Castro <aurelio.cos@outlook.com>
2022-06-29 10:21:00 -0300

    Fix Erro Page Vercel:v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { ApolloProvider } from "@apollo/client";
-import { Layout } from "./components/Layout";
 import { AuthProvider } from "./contexts/AuthContext";
 import  Router from "./Router";
 import { client } from './lib/apollo'
@@ -8,9 +7,7 @@ export const App = () => {
   return (
     <ApolloProvider client={client}>
       <AuthProvider >
-        <Layout>
           <Router />
-        </Layout>
       </AuthProvider>
     </ApolloProvider>
   )
